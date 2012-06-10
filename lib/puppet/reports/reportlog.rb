cb514d5bb1b75dbab584727fbc5383d2444385c1
Wil Cooley <wcooley@pdx.edu>
2012-06-10 00:34:38 -0700

    Adds beginning of experimental report processing

---
+++ lib/puppet/reports/reportlog.rb
@@ -0,0 +1,15 @@
+require 'puppet'
+Puppet::Reports.register_report(:reportlog) do
+  desc <<-DESC
+  Logs more useful information than the default 'log' report, such as metrics.
+DESC
+
+  def process
+    File.open('/tmp/puppet-reportlog.log') do |f|
+      self.metrics.each do |name, metric|
+        f.puts("name=#{name} metric=#{metric}")
+      end
+    end
+  end
+end
+
