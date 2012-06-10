53b7d532ab71c77513fec1e532705097e389a509
Wil Cooley <wcooley@pdx.edu>
2012-06-10 00:50:07 -0700

    Maybe puppet doesn't want to let me write wherever?

--- lib/puppet/reports/reportlog.rb
+++ lib/puppet/reports/reportlog.rb
@@ -5,11 +5,18 @@ Puppet::Reports.register_report(:reportlog) do
 DESC
 
   def process
-    File.open('/tmp/puppet-reportlog.log', 'w') do |f|
+    dir = File.join(Puppet[:reportdir], self.host)
+    Dir.mkdir(dir) unless File.exists?(dir)
+    file = config_version + ".logs"
+    destination = File.join(dir, file)
+    File.open(destination,"w") do |f|
+#      f.write(logs)
       self.metrics.each do |name, metric|
         f.puts("name=#{name} metric=#{metric}")
       end
     end
+#    File.open('/tmp/puppet-reportlog.log', 'w') do |f|
+#    end
   end
 end
 
