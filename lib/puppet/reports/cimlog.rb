27423cc72bac571ad0463880f8e6404f83590252
Wil Cooley <wcooley@nakedape.cc>
2012-06-16 23:36:45 -0700

    Adds & enhances logging
    * Uses current Unix timestamp as a run identifier. The
      configuration_version is not appropriate because it does not change if
      the configuration itself does not change, so it makes it difficult to
      build a transaction out of.
    
    * Noticed that the Puppet.<level> methods join the arguments with a
      space, so I don't need to do that myself.
    
    * Split into several methods to make it easier to work with.
    
    * Added "base" method, which logs global report information, such as
      configuration_version, etc.
    
    * Adds logs method, which capture the agent's log messages (like the
      'log' report, only CIMmier. This is disabled currently, as I am
      uncertain of its value.
    
    * Adds resource_statuses methods, which logs changed resources and
      associated events.

--- lib/puppet/reports/cimlog.rb
+++ lib/puppet/reports/cimlog.rb
@@ -1,17 +1,32 @@
-require 'puppet/reports'
+require 'puppet'
 
 Puppet::Reports.register_report(:cimlog) do
   desc "Send metrics to log, structured to make it easy with Splunk."
 
   def process
-    node = self.host.split('.', 2)[0]
+    cimlog_base
+    cimlog_metrics
+#    cimlog_logs
+    cimlog_resource_statuses
+  end
 
-    prefix_msg = "[cimlog] node=#{node} ver=\"#{self.configuration_version.to_s}\""
+  def cimlog_base
+    @node = self.host.split('.', 2)[0]
+    @run_id = Time.now.to_i.to_s if not @run_id
 
+    @prefix_msg = "[cimlog] node=#{@node} id=#{@run_id}"
+    Puppet.notice(@prefix_msg, %W{
+                  ver="#{self.configuration_version.to_s}"
+                  kind="#{self.kind}"
+                  status="#{self.status}"
+                })
+  end
+
+  def cimlog_metrics
     self.metrics.each do |category,data|
 
       msg = Array.new
-      msg += [ prefix_msg ]
+      msg += [ @prefix_msg ]
       msg += [ 'category="%s"' % category ]
 
       if category == 'time'
@@ -25,8 +40,45 @@ Puppet::Reports.register_report(:cimlog) do
         '%s="%s"' % [metric, value]
       end
 
-      Puppet.info(msg.join(' '))
+      Puppet.notice(msg)
     end
   end
+
+  def cimlog_logs
+    self.logs.each do |log|
+      Puppet.notice(@prefix_msg, %W{
+                    source="#{log.source}"
+                    level="#{log.level}"
+                    msg="#{log.to_s}"
+                  })
+    end
+  end
+
+  def cimlog_resource_statuses
+    resources = self.resource_statuses.values.select { |r| r.change_count > 0 }
+
+    resources.each do |res|
+
+      Puppet.notice(@prefix_msg, %W{
+                      resource="#{res.resource}"
+                      resource_title="#{res.title}"
+                      resource_type="#{res.resource_type}"
+                      resource_events="#{res.events.length}"
+                    })
+
+      res.events.each do |event|
+        Puppet.notice(@prefix_msg, %W{
+                      resource="#{res.resource}"
+                      event_name="#{event.name}"
+                      event_property="#{event.property}"
+                      event_status="#{event.status}"
+                      desired_value="#{event.desired_value}"
+                      previous_value="#{event.previous_value}"
+                    })
+                      
+      end
+    end
+  end
+
 end
 
